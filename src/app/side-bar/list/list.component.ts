3105607704eb21e8250eb2c0172e70cdb00ecb5a
tsheringyangden <tshering.yangden@selise.ch>
2022-12-30 13:43:02 +0600

    feat: removed comment

---
+++ src/app/side-bar/list/list.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss']
+})
+export class ListComponent {
+  // products: Product[];
+  products: any = [
+    {
+      list_item: 'SEO Agiencies',
+      create_on: '21 May 2022',
+    },
+    {
+      list_item: 'SEO Agiencies',
+      create_on: '21 May 2022',
+    },
+    {
+      list_item: 'SEO Agiencies',
+      create_on: '21 May 2022',
+    },
+    {
+      list_item: 'SEO Agiencies',
+      create_on: '21 May 2022',
+    },
+    {
+      list_item: 'SEO Agiencies',
+      create_on: '21 May 2022',
+    },
+    {
+      list_item: 'SEO Agiencies',
+      create_on: '21 May 2022',
+    },
+    {
+      list_item: 'SEO Agiencies',
+      create_on: '21 May 2022',
+    }
+  ]
+
+
+
+
+
+
+
+}
