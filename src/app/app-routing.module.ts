51af17dfd0e058b747058aa4cb9d130b78816a4a
tsheringyangden <tshering.yangden@selise.ch>
2023-01-06 11:02:31 +0600

    fix: removed double quotation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import {DashboardComponent} from "./side-bar/dashboard/dashboard.component";
-import {ListComponent} from "./side-bar/list/list.component";
+import {RouterModule, Routes} from '@angular/router';
+import {DashboardComponent} from './side-bar/dashboard/dashboard.component';
+import {ListComponent} from './side-bar/list/list.component';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent},
