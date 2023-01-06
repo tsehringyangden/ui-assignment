ffa8694cd93a7b16fd4c259230a7af9681a2465d
tsheringyangden <tshering.yangden@selise.ch>
2023-01-06 10:59:11 +0600

    fix: removed double quotation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {SplitButtonModule} from 'primeng/splitbutton';
 import {AccordionModule} from 'primeng/accordion';
 import {FormsModule} from '@angular/forms';
 import {TabMenuModule} from 'primeng/tabmenu';
 import {FlexModule} from '@angular/flex-layout';
-import { SideBarComponent } from './side-bar/side-bar.component';
+import {SideBarComponent} from './side-bar/side-bar.component';
 import {RatingModule} from 'primeng/rating';
 import { HeaderComponent } from './header/header.component';
 import {RippleModule} from 'primeng/ripple';
 import { SubHeaderComponent } from './sub-header/sub-header.component';
-import { DashboardComponent } from './side-bar/dashboard/dashboard.component';
+import {DashboardComponent} from './side-bar/dashboard/dashboard.component';
 import {ChartModule} from 'primeng/chart';
 import {ImageModule} from 'primeng/image';
-import { ListComponent } from './side-bar/list/list.component';
+import {ListComponent} from './side-bar/list/list.component';
 import {TableModule} from 'primeng/table';
 import {CheckboxModule} from 'primeng/checkbox';
-import {PaginatorModule} from "primeng/paginator";
-import { SubHeaderListComponent } from './side-bar/list/sub-header-list/sub-header-list.component';
-import {InputTextModule} from "primeng/inputtext";
-import {AvatarModule} from "primeng/avatar";
-import { ListHeaderComponent } from './side-bar/list/list-header/list-header.component';
+import {PaginatorModule} from 'primeng/paginator';
+import {SubHeaderListComponent} from './side-bar/list/sub-header-list/sub-header-list.component';
+import {InputTextModule} from 'primeng/inputtext';
+import {AvatarModule} from 'primeng/avatar';
+import {ListHeaderComponent} from './side-bar/list/list-header/list-header.component';
 
 @NgModule({
   declarations: [
